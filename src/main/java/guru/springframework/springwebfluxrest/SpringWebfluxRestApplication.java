9bdb25dd849e20fb4ac40cd6bf17d4985f22c326
Nate Walters <newsproutsmedia@gmail.com>
2018-10-23 19:57:25 -0700

    Initial commit

---
+++ src/main/java/guru/springframework/springwebfluxrest/SpringWebfluxRestApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.springwebfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxRestApplication.class, args);
+    }
+}
---
+++ src/test/java/guru/springframework/springwebfluxrest/SpringWebfluxRestApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.springwebfluxrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
